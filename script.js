d72d798d11924d5e70012a3c4d8a407db1b80fd6
thanmaireddy-dev <thanmaireddy77@gmail.com>
2025-12-16 17:19:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - " 
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset() {
+    if (count === 0 && saveEl.textContent === "Previous entries: ") return
+    count = 0
+    countEl.textContent = 0
+    saveEl.textContent = "Previous entries: "
+}
+
